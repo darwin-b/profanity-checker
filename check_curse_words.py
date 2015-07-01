202e528fe1dbfcd30e7eeaa4c7f9aeac6c94c3a7
darwin-b <darwin.bollepalli@gmail.com>
2015-07-01 15:34:15 +0530

    executable file
    code that checks for presence of curse words in the document check.txt
    file

---
+++ check_curse_words.py
@@ -0,0 +1,26 @@
+import urllib
+import os
+def read_text():
+    path = os.getcwd()
+    path = path+"\check.txt"
+    quotes = open(path) 
+    contents_of_file = quotes.read()
+    #print(contents_of_file)
+    quotes.close()
+    check_profanity(contents_of_file)    
+    
+def check_profanity(text_to_check):
+    connection = urllib.urlopen("http://www.wdyl.com/profanity?q="+text_to_check)
+    output = connection.read()
+    #print(output)
+    
+    connection.close()
+    if "true" in output:
+        print ("Profanity Alert - found cousre words")
+    elif "false" in output:
+        print "This Document has no curse words"
+    else:
+        print "could'nt scan document properly"
+    
+read_text()
+
